465fdf0a8fb0d7036389cc3c3a0c8ce8bc073a39
dnowak2 <60153413+dnowak2@users.noreply.github.com>
2020-03-24 19:23:26 -0500

    Create FileGrabber.java

---
+++ src/test/java/webpages/FileGrabber.java
@@ -0,0 +1,17 @@
+package files;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class FileGrabber 
+{
+	protected WebDriver driver;
+	
+	public static void main(String[] args)
+	{
+		
+		driver = new geckodriver();
+		
+		
+	}
+}
