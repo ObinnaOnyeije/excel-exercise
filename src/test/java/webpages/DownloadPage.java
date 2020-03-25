a89a5a245de1019391076d38dd063d8fc249f067
ObinnaOnyeije <obinnaonyeije@yahoo.com>
2020-03-24 20:15:24 -0400

    Added download link

--- src/test/java/webpages/DownloadPage.java
+++ src/test/java/webpages/DownloadPage.java
@@ -6,8 +6,15 @@ import org.openqa.selenium.support.FindBy;
 
 public class DownloadPage extends PageObject {
 
+	@FindBy(xpath = "//*[@id=\"dataset-resources\"]/div/ul/li[2]/span[1]/a")
+	private WebElement downloadLink;
+	
 	public DownloadPage(WebDriver driver) {
 		super(driver);
+	}
+	
+	public void clickDownloadLink() {
+		downloadLink.click();
 	}
 	
 
